f3fcdcfd9adcc6617d99f2aa2caf54585ae0c7ce
Lotfi Waderni <waderni.lotfi@gmail.com>
2021-09-04 23:20:24 +0200

    Create main.tf

---
+++ examples/complete/main.tf
@@ -0,0 +1,18 @@
+module "example" {
+  # source = "github.com/faudeltn/asg-dns-handler.git=v1.1"
+    source = "git::https://github.com/faudeltn/asg-dns-handler.git?ref=v1.4"
+    
+    autoscale_route53zone_id   = local.internal_zone_id
+    domain = local.internal_zone_name
+
+    autoscale_handler_unique_identifier = "holla-world-asg-dns-autoscale"
+    # autoscale_handler_unique_identifier = local.autoscale_handler_unique_identifier[terraform.workspace]
+
+    tags = {
+        Customer             = "YALLALABS"
+        Terraform            = "true"
+        # Name                 = "Prova"
+        @ Environment          = upper(local.env_name[terraform.workspace])
+        Environment          = "DEV"
+    }
+}
